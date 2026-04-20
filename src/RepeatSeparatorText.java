03d98d796739ed54e573bec32ec256e7572ef84d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:37:26 +0400

    Implement separator logic in Task 12

--- src/RepeatSeparatorText.java
+++ src/RepeatSeparatorText.java
@@ -2,7 +2,10 @@ public class RepeatSeparatorText {
     public static String repeatSeparator(String mainWord, String separatorText, int repeatCount){
         String resultText = "";
             for (int index = 0; index < repeatCount; index++){
-
+                if (index >0){
+                    resultText += separatorText;
+                }
+                resultText += mainWord;
             }
 
         return resultText;
