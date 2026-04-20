6612d6b1edfe5faa45e89b2e8509bff586c33b0f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:28:48 +0400

    Implement front repetition logic in Task 11

--- src/RepeatFrontText.java
+++ src/RepeatFrontText.java
@@ -2,7 +2,7 @@ public class RepeatFrontText {
     public static String repeatFront(String originalText, int frontLength){
         String resultText = "";
         for (int index = frontLength; index > 0; index--){
-
+            resultText += originalText.substring(0, index);
         }
         return resultText;
     }
