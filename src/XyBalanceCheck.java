a11bcaaec8e9ffc03581a0cc31af7678d67a183a
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:27:49 +0400

    Implement x and y balance logic in Task 8

--- src/XyBalanceCheck.java
+++ src/XyBalanceCheck.java
@@ -1,6 +1,19 @@
 public class XyBalanceCheck {
     public static boolean xyBalance(String originalText) {
-        return true;
+        int XlastP = -1;
+        int YlastP = -1;
+
+        for (int index = 0; index < originalText.length(); index++) {
+            if (originalText.charAt(index) == 'x') {
+                XlastP = index;
+            }
+
+            if (originalText.charAt(index) == 'y') {
+                YlastP = index;
+            }
+        }
+
+        return XlastP <= YlastP;
     }
     public static void main (String[] args){
 
