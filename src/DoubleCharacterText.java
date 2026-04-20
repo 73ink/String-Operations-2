a1af00c46441cf9422deb1844279c6eac605a280
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 11:43:58 +0400

    Implement character doubling logic in Task 1

--- src/DoubleCharacterText.java
+++ src/DoubleCharacterText.java
@@ -2,6 +2,11 @@ public class DoubleCharacterText {
 
     public static String doubleChar(String originalText) {
         String doubledText = "";
+        for (int index = 0; index < originalText.length(); index++) {
+            char currentCharacter = originalText.charAt(index);
+            doubledText += currentCharacter;
+            doubledText += currentCharacter;
+        }
         return doubledText;
     }
 
