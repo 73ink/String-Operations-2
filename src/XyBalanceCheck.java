150d170fae1eabe425e40ed59651b0abe69bb4e5
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:28:47 +0400

    Add example cases and finalize Task 8

--- src/XyBalanceCheck.java
+++ src/XyBalanceCheck.java
@@ -16,6 +16,8 @@ public class XyBalanceCheck {
         return XlastP <= YlastP;
     }
     public static void main (String[] args){
-
+        System.out.println(xyBalance("aaxbby"));
+        System.out.println(xyBalance("aaxbb"));
+        System.out.println(xyBalance("yaaxbb"));
     }
 }
