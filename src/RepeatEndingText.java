e5e264c733e1a2cf68a08529d9cf0b6886862fd6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:18:37 +0400

    Add extraction of ending part in Task 10

--- src/RepeatEndingText.java
+++ src/RepeatEndingText.java
@@ -1,6 +1,7 @@
 public class RepeatEndingText {
     public static String repeatEnd(String originalText, int repeatCount){
         String repeatedText = "";
+        String endingPart = originalText.substring(originalText.length() - repeatCount);
         return repeatedText;
 
     }
