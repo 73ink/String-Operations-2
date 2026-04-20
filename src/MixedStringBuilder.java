7858769debfa03f02de21ac915f907a7764c41d0
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:32:56 +0400

    Implement alternating merge logic in Task 9

--- src/MixedStringBuilder.java
+++ src/MixedStringBuilder.java
@@ -4,7 +4,13 @@ public class MixedStringBuilder {
         int maximumLength = Math.max(firstText.length(), secondText.length());
 
         for (int index = 0; index < maximumLength; index++) {
+            if (index < firstText.length()) {
+                mixedText += firstText.charAt(index);
+            }
 
+            if (index < secondText.length()) {
+                mixedText += secondText.charAt(index);
+            }
         }
         return mixedText;
     }
