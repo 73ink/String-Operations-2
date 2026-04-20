5517ad746b2db536e1281b37c349bd8975ec8958
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:18:27 +0400

    Add loop to scan b?b patterns in Task 7

--- src/BobPatternCheck.java
+++ src/BobPatternCheck.java
@@ -1,5 +1,7 @@
 public class BobPatternCheck {
     public static boolean bobThere(String originalText) {
+        for (int index = 0; index < originalText.length() - 2; index++) {
+        }
         return false;
     }
     public static void main (String[] args){
