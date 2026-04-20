a88d28cec4f1d84f622be24d75f0b3703d7d8faf
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:26:12 +0400

    Add decreasing loop for Task 11

--- src/RepeatFrontText.java
+++ src/RepeatFrontText.java
@@ -1,6 +1,9 @@
 public class RepeatFrontText {
     public static String repeatFront(String originalText, int frontLength){
         String resultText = "";
+        for (int index = frontLength; index > 0; index--){
+
+        }
         return resultText;
     }
     public static void main (String[] args){
