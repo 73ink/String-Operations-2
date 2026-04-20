f295efae3de530ad30339613e8449704efa443a7
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:18:59 +0400

    Implement b?b pattern logic in Task 7

--- src/BobPatternCheck.java
+++ src/BobPatternCheck.java
@@ -1,6 +1,9 @@
 public class BobPatternCheck {
     public static boolean bobThere(String originalText) {
         for (int index = 0; index < originalText.length() - 2; index++) {
+            if (originalText.charAt(index) == 'b' && originalText.charAt(index + 2) == 'b') {
+                return true;
+            }
         }
         return false;
     }
