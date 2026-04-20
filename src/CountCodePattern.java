7def945de4673e555bd4ee806d69a0bfaf08d092
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:20:11 +0400

    Add loop to scan code pattern positions

--- src/CountCodePattern.java
+++ src/CountCodePattern.java
@@ -1,6 +1,8 @@
 public class CountCodePattern {
     public static int countCode(String originalText) {
         int patternCount = 0;
+        for (int index = 0; index < originalText.length() - 3; index++) {
+        }
         return patternCount;
     }
 
