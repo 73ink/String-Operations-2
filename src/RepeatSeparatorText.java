66db9bd72412eedbeb7f7240365b42282a1c7605
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:34:34 +0400

    Add repeatSeparator method for Task 12

--- src/RepeatSeparatorText.java
+++ src/RepeatSeparatorText.java
@@ -1,4 +1,8 @@
 public class RepeatSeparatorText {
+    public static String repeatSeparator(String mainWord, String separatorText, int repeatCount){
+        String resultText = "";
+        return resultText;
+    }
     public static void main (String[] args){
 
     }
