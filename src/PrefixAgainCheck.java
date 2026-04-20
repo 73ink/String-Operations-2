54bdc3b3df818918de364921c488bfda4d5414ea
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:50:46 +0400

    Implement repeated prefix check in Task 13

--- src/PrefixAgainCheck.java
+++ src/PrefixAgainCheck.java
@@ -2,7 +2,10 @@ public class PrefixAgainCheck {
     public static boolean prefixAgain(String originalText, int prefixLength){
         String prefixText = originalText.substring(0, prefixLength);
             for (int index = prefixLength; index <= originalText.length() - prefixLength; index++){
-
+                String currentPart = originalText.substring(index, index + prefixLength);
+                    if (currentPart.equals(prefixText)){
+                        return true;
+                    }
             }
         return false;
     }
