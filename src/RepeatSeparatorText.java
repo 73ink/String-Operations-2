8496ff9fb7e44a77c394b8e8fa3405c7f8787c9a
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:36:00 +0400

    Add repetition loop for Task 12

--- src/RepeatSeparatorText.java
+++ src/RepeatSeparatorText.java
@@ -1,6 +1,10 @@
 public class RepeatSeparatorText {
     public static String repeatSeparator(String mainWord, String separatorText, int repeatCount){
         String resultText = "";
+            for (int index = 0; index < repeatCount; index++){
+
+            }
+
         return resultText;
     }
     public static void main (String[] args){
