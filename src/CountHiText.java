e9c1a40452ecb75690546fa909cbea8c488f9797
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:07:45 +0400

    Implement hi counting logic in Task 2

--- src/CountHiText.java
+++ src/CountHiText.java
@@ -2,7 +2,11 @@ public class CountHiText {
     public static int countHi(String originalText) {
         int hiCount = 0;
         for (int index = 0; index < originalText.length() - 1; index++) {
+            String currentPart = originalText.substring(index, index + 2);
 
+            if (currentPart.equals("hi")) {
+                hiCount++;
+            }
         }
         return hiCount;
     }
