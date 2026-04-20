790ab1d7cbb4833a85bd5aafc0f05144dd4ea9c6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:33:51 +0400

    Add lowercase conversion for case-insensitive comparison

--- src/EndingTextCheck.java
+++ src/EndingTextCheck.java
@@ -1,5 +1,7 @@
 public class EndingTextCheck {
     public static boolean endOther(String firstText, String secondText){
+        String firstLowerText = firstText.toLowerCase();
+        String secondLowerText = secondText.toLowerCase();
         return  false;
     }
     public static void main (String[] args){
