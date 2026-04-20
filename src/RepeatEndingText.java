3edfeaa1f4a4b02842ec319d1a1d9707458ad9b2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:17:14 +0400

    Add repeatEnd method for Task 10

--- src/RepeatEndingText.java
+++ src/RepeatEndingText.java
@@ -1,5 +1,9 @@
 public class RepeatEndingText {
+    public static String repeatEnd(String originalText, int repeatCount){
+        String repeatedText = "";
+        return repeatedText;
 
+    }
     public static void main (String[] args){
 
     }
