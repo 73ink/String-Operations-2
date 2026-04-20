2c78c3030a3219fd949aa9f2ac32247aa48c3907
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:11:51 +0400

    Add catDog method and counters for Task 3

--- src/CatDogBalanceCheck.java
+++ src/CatDogBalanceCheck.java
@@ -1,4 +1,9 @@
 public class CatDogBalanceCheck {
+    public static boolean catDog(String originalText) {
+        int catCount = 0;
+        int dogCount = 0;
+        return catCount == dogCount;
+    }
     public static void main(String[] args) {
 
     }
