5ed353993cdcf07c3c4f6a224bc5931f479c4e0f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:33:12 +0400

    Add endOther method for Task 5

--- src/EndingTextCheck.java
+++ src/EndingTextCheck.java
@@ -1,4 +1,7 @@
 public class EndingTextCheck {
+    public static boolean endOther(String firstText, String secondText){
+        return  false;
+    }
     public static void main (String[] args){
 
     }
