25a656da258e97ad9d41d1ffa45c46bcde33a014
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:19:56 +0400

    Implement repeated ending logic in Task 10

--- src/RepeatEndingText.java
+++ src/RepeatEndingText.java
@@ -2,6 +2,9 @@ public class RepeatEndingText {
     public static String repeatEnd(String originalText, int repeatCount){
         String repeatedText = "";
         String endingPart = originalText.substring(originalText.length() - repeatCount);
+        for (int index =0; index < repeatCount; index++){
+            repeatedText += endingPart;
+        }
         return repeatedText;
 
     }
