3b9ad1ee6e5a24146ea0055f21dbc6d1c05c2384
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:13:45 +0400

    Add loop to scan 3-letter words in Task 3

--- src/CatDogBalanceCheck.java
+++ src/CatDogBalanceCheck.java
@@ -2,6 +2,9 @@ public class CatDogBalanceCheck {
     public static boolean catDog(String originalText) {
         int catCount = 0;
         int dogCount = 0;
+            for (int index = 0; index < originalText.length() - 2; index++) {
+
+            }
         return catCount == dogCount;
     }
     public static void main(String[] args) {
