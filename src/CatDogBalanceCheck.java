2502244384895254546417d8e48043a3aac3b7f7
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:14:41 +0400

    Add cat and dog matching logic in Task 3

--- src/CatDogBalanceCheck.java
+++ src/CatDogBalanceCheck.java
@@ -3,7 +3,15 @@ public class CatDogBalanceCheck {
         int catCount = 0;
         int dogCount = 0;
             for (int index = 0; index < originalText.length() - 2; index++) {
+                String currentPart = originalText.substring(index, index + 3);
 
+                if (currentPart.equals("cat")) {
+                    catCount++;
+                }
+
+                if (currentPart.equals("dog")) {
+                    dogCount++;
+                }
             }
         return catCount == dogCount;
     }
