ae26dfec67dec688bbc20c6cb1fb952e78fb4b98
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:56:41 +0400

    Add loop to search xyz positions in Task 14

--- src/XyzMiddleCheck.java
+++ src/XyzMiddleCheck.java
@@ -1,5 +1,8 @@
 public class XyzMiddleCheck {
     public static boolean xyzMiddle(String originalText){
+        for (int index = 0; index <= originalText.length() - 3; index++){
+
+        }
         return false;
     }
     public static void main(String[] args){
