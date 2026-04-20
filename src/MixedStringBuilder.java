dec6ce8c456a7eeee248177366413c084f219b80
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:34:00 +0400

    Add sample outputs and finalize Task 9

--- src/MixedStringBuilder.java
+++ src/MixedStringBuilder.java
@@ -15,6 +15,8 @@ public class MixedStringBuilder {
         return mixedText;
     }
     public static void main (String[] args){
-
+        System.out.println(mixString("abc", "xyz"));
+        System.out.println(mixString("Hi", "There"));
+        System.out.println(mixString("xxxx", "There"));
     }
 }
