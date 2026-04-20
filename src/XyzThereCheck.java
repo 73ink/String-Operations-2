857b26bc34f5e643907bfeb5efd90eb6634370b2
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:13:48 +0400

    Add loop to scan xyz positions in Task 6

--- src/XyzThereCheck.java
+++ src/XyzThereCheck.java
@@ -1,5 +1,7 @@
 public class XyzThereCheck {
     public static boolean xyzThere(String originalText){
+        for (int index = 0; index < originalText.length() - 2; index++) {
+        }
         return false;
     }
     public static void main (String[] args){
