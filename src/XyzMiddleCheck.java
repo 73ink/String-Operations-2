af35fff5d005563983090cc84b328808ec9c1ce1
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 16:00:45 +0400

    Implement middle xyz logic in Task 14

--- src/XyzMiddleCheck.java
+++ src/XyzMiddleCheck.java
@@ -1,7 +1,14 @@
 public class XyzMiddleCheck {
     public static boolean xyzMiddle(String originalText){
         for (int index = 0; index <= originalText.length() - 3; index++){
+            if (originalText.substring(index, index + 3).equals("xyz")){
+                int leftSideLength = index;
+                int rightSideLength = originalText.length() - (index + 3);
 
+                if (Math.abs(leftSideLength - rightSideLength) <= 1){
+                    return true;
+                }
+            }
         }
         return false;
     }
