f952b0cd8278e070356afcf918e3be97f1670d54
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:48:48 +0400

    Add prefix extraction and loop for Task 13

--- src/PrefixAgainCheck.java
+++ src/PrefixAgainCheck.java
@@ -1,5 +1,9 @@
 public class PrefixAgainCheck {
     public static boolean prefixAgain(String originalText, int prefixLength){
+        String prefixText = originalText.substring(0, prefixLength);
+            for (int index = prefixLength; index <= originalText.length() - prefixLength; index++){
+
+            }
         return false;
     }
     public static void main (String[] args){
