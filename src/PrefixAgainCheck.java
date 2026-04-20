fd5f2d86484747bfe5e6391bd70e0eea5e86e28e
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:46:15 +0400

    Add prefixAgain method for Task 13

--- src/PrefixAgainCheck.java
+++ src/PrefixAgainCheck.java
@@ -1,4 +1,7 @@
 public class PrefixAgainCheck {
+    public static boolean prefixAgain(String originalText, int prefixLength){
+        return false;
+    }
     public static void main (String[] args){
 
     }
