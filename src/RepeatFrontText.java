11f514c1c3462cabe9ef2f728a8b3a917ee2cd5c
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 15:24:34 +0400

    Add repeatFront method for Task 11

--- src/RepeatFrontText.java
+++ src/RepeatFrontText.java
@@ -1,4 +1,8 @@
 public class RepeatFrontText {
+    public static String repeatFront(String originalText, int frontLength){
+        String resultText = "";
+        return resultText;
+    }
     public static void main (String[] args){
 
     }
