c0baa40cc5d0cfd735efbe8a6b62b809501e4439
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:06:47 +0400

    Add loop to scan text in Task 2

--- src/CountHiText.java
+++ src/CountHiText.java
@@ -1,6 +1,9 @@
 public class CountHiText {
     public static int countHi(String originalText) {
         int hiCount = 0;
+        for (int index = 0; index < originalText.length() - 1; index++) {
+
+        }
         return hiCount;
     }
 
