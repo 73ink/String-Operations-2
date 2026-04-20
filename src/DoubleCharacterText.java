4535b9d71cee1a1c9e0ed88879a8f3065cf3e148
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 11:42:55 +0400

    Add doubleChar method for Task 1

--- src/DoubleCharacterText.java
+++ src/DoubleCharacterText.java
@@ -1,5 +1,10 @@
 public class DoubleCharacterText {
 
+    public static String doubleChar(String originalText) {
+        String doubledText = "";
+        return doubledText;
+    }
+
     public static void main(String[] args) {
 
     }
