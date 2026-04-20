a4c3d276a36b511ae8e4ea88e84a0c09a00454de
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 12:18:23 +0400

    Add countCode method for Task 4

--- src/CountCodePattern.java
+++ src/CountCodePattern.java
@@ -1,4 +1,8 @@
 public class CountCodePattern {
+    public static int countCode(String originalText) {
+        int patternCount = 0;
+        return patternCount;
+    }
 
     public static void main (String[] args){
 
