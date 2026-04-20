4a9d7ee806cafdcd63c34847062a6127bc7d20b6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-20 13:32:11 +0400

    Add loop for alternating characters in Task 9

--- src/MixedStringBuilder.java
+++ src/MixedStringBuilder.java
@@ -1,6 +1,11 @@
 public class MixedStringBuilder {
     public static String mixString(String firstText, String secondText) {
         String mixedText = "";
+        int maximumLength = Math.max(firstText.length(), secondText.length());
+
+        for (int index = 0; index < maximumLength; index++) {
+
+        }
         return mixedText;
     }
     public static void main (String[] args){
