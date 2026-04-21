b50456c90631c29d27c028c59cbf6dbc3e71f7a8
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:03:29 +0400

    Implement oneTwo shifting logic in Task 17

--- src/OneTwoTextShift.java
+++ src/OneTwoTextShift.java
@@ -2,6 +2,9 @@ public class OneTwoTextShift {
     public static String oneTwo(String originalText) {
         String shiftedText = "";
         for (int index = 0; index <= originalText.length() - 3; index += 3) {
+            shiftedText += originalText.charAt(index + 1);
+            shiftedText += originalText.charAt(index + 2);
+            shiftedText += originalText.charAt(index);
         }
         return shiftedText;
     }
