ecec7928e89af4f225970a943a662a569ac4013f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:43:02 +0400

    Add loop to scan word positions in Task 21

--- src/WordEndsCollector.java
+++ src/WordEndsCollector.java
@@ -1,6 +1,9 @@
 public class WordEndsCollector {
     public static String wordEnds(String originalText, String targetWord) {
         String collectedText = "";
+        for (int index = 0; index <= originalText.length() - targetWord.length(); index++) {
+
+        }
         return collectedText;
     }
     public static void main (String[] args){
