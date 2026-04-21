5333dda7bad2f1d17a923d8399a52266dbaae79e
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:32:55 +0400

    Implement star and neighbor removal in Task 19

--- src/StarOutCleaner.java
+++ src/StarOutCleaner.java
@@ -2,7 +2,19 @@ public class StarOutCleaner {
     public static String starOut(String originalText) {
         String cleanedText = "";
             for (int index = 0; index < originalText.length(); index++) {
+                if (originalText.charAt(index) == '*') {
+                    continue;
+                }
 
+                if (index > 0 && originalText.charAt(index - 1) == '*') {
+                    continue;
+                }
+
+                if (index < originalText.length() - 1 && originalText.charAt(index + 1) == '*') {
+                    continue;
+                }
+
+                cleanedText += originalText.charAt(index);
             }
         return cleanedText;
     }
