e8543bda1c68cd53a4a03a4027a8a76e571af726
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:02:37 +0400

    Add group loop for Task 17

--- src/OneTwoTextShift.java
+++ src/OneTwoTextShift.java
@@ -1,6 +1,8 @@
 public class OneTwoTextShift {
     public static String oneTwo(String originalText) {
         String shiftedText = "";
+        for (int index = 0; index <= originalText.length() - 3; index += 3) {
+        }
         return shiftedText;
     }
     public static void main (String[] args){
