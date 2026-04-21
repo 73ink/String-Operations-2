744bf276d578ff73df408ea7bc7208daa95cd057
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:29:19 +0400

    Add character loop for Task 19

--- src/StarOutCleaner.java
+++ src/StarOutCleaner.java
@@ -1,6 +1,9 @@
 public class StarOutCleaner {
     public static String starOut(String originalText) {
         String cleanedText = "";
+            for (int index = 0; index < originalText.length(); index++) {
+
+            }
         return cleanedText;
     }
     public static void main (String[] args){
