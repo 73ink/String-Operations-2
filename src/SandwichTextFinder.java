f5288f1708678d045723e3bc21a00ff960da24cd
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 09:53:54 +0400

    Implement sandwich text extraction in Task 15

--- src/SandwichTextFinder.java
+++ src/SandwichTextFinder.java
@@ -2,6 +2,10 @@ public class SandwichTextFinder {
     public static String getSandwich(String originalText) {
         int firstBreadPosition = originalText.indexOf("bread");
         int lastBreadPosition = originalText.lastIndexOf("bread");
+
+        if (firstBreadPosition != -1 && lastBreadPosition != -1 && firstBreadPosition != lastBreadPosition) {
+            return originalText.substring(firstBreadPosition + 5, lastBreadPosition);
+        }
         return "";
     }
     public static void main (String[] args){
