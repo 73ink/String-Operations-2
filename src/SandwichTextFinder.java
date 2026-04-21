fdef1c6ae89f78c984cdafb9a430c3142731935b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 09:54:35 +0400

    Add example tests and finalize Task 15

--- src/SandwichTextFinder.java
+++ src/SandwichTextFinder.java
@@ -9,6 +9,8 @@ public class SandwichTextFinder {
         return "";
     }
     public static void main (String[] args){
-
+        System.out.println(getSandwich("breadjambread"));
+        System.out.println(getSandwich("xxbreadjambreadyy"));
+        System.out.println(getSandwich("xxbreadyy"));
     }
 }
