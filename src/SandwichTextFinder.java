5c191145cec602eb54b1131c8f544f8cd90b6d60
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 08:32:12 +0400

    Add first and last bread positions in Task 15

--- src/SandwichTextFinder.java
+++ src/SandwichTextFinder.java
@@ -1,5 +1,7 @@
 public class SandwichTextFinder {
     public static String getSandwich(String originalText) {
+        int firstBreadPosition = originalText.indexOf("bread");
+        int lastBreadPosition = originalText.lastIndexOf("bread");
         return "";
     }
     public static void main (String[] args){
