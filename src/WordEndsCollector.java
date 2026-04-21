78c3eacefe2b869bd98fa5c4fb11371fc5e1b66d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:43:33 +0400

    Implement surrounding character collection in Task 21

--- src/WordEndsCollector.java
+++ src/WordEndsCollector.java
@@ -2,7 +2,15 @@ public class WordEndsCollector {
     public static String wordEnds(String originalText, String targetWord) {
         String collectedText = "";
         for (int index = 0; index <= originalText.length() - targetWord.length(); index++) {
+            if (originalText.substring(index, index + targetWord.length()).equals(targetWord)) {
+                if (index > 0) {
+                    collectedText += originalText.charAt(index - 1);
+                }
 
+                if (index + targetWord.length() < originalText.length()) {
+                    collectedText += originalText.charAt(index + targetWord.length());
+                }
+            }
         }
         return collectedText;
     }
