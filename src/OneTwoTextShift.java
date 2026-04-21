05aa666b7e281fc383ba35fa6e4768a8580387bb
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:02:01 +0400

    Add oneTwo method for Task 17

--- src/OneTwoTextShift.java
+++ src/OneTwoTextShift.java
@@ -1,4 +1,8 @@
 public class OneTwoTextShift {
+    public static String oneTwo(String originalText) {
+        String shiftedText = "";
+        return shiftedText;
+    }
     public static void main (String[] args){
 
     }
