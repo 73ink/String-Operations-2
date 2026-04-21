aaa30a216f6cba0d07303987d26377dae89c08df
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:06:26 +0400

    Add zipZap method for Task 18

--- src/ZipZapCleaner.java
+++ src/ZipZapCleaner.java
@@ -1,4 +1,8 @@
 public class ZipZapCleaner {
+    public static String zipZap(String originalText) {
+        String cleanedText = "";
+        return cleanedText;
+    }
     public static void main (String[] args){
 
     }
