35eb186cc6b0b36850f13c35c7b008f1401eb354
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 09:57:37 +0400

    Add loop to scan star positions in Task 16

--- src/SameStarCharacterCheck.java
+++ src/SameStarCharacterCheck.java
@@ -1,5 +1,7 @@
 public class SameStarCharacterCheck {
     public static boolean sameStarChar(String originalText) {
+        for (int index = 1; index < originalText.length() - 1; index++) {
+        }
         return true;
     }
     public static void main(String[] args){
