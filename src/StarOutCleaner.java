8266cdc8a0691cfffc84c6a66a7ca1c4b2b1ecfc
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:25:15 +0400

    Add starOut method for Task 19

--- src/StarOutCleaner.java
+++ src/StarOutCleaner.java
@@ -1,4 +1,8 @@
 public class StarOutCleaner {
+    public static String starOut(String originalText) {
+        String cleanedText = "";
+        return cleanedText;
+    }
     public static void main (String[] args){
 
     }
