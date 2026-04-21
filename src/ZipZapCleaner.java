f07e744b4fe264c9bec1d31782a09b415742db07
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:07:09 +0400

    Add loop structure for Task 18

--- src/ZipZapCleaner.java
+++ src/ZipZapCleaner.java
@@ -1,7 +1,10 @@
 public class ZipZapCleaner {
     public static String zipZap(String originalText) {
         String cleanedText = "";
-        return cleanedText;
+        for (int index = 0; index < originalText.length(); index++) {
+
+        }
+            return cleanedText;
     }
     public static void main (String[] args){
 
