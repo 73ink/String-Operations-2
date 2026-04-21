9f00d425cab379b325a465199c4d6b9834cd0ecf
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 09:56:34 +0400

    Add sameStarChar method for Task 16

--- src/SameStarCharacterCheck.java
+++ src/SameStarCharacterCheck.java
@@ -1,4 +1,7 @@
 public class SameStarCharacterCheck {
+    public static boolean sameStarChar(String originalText) {
+        return true;
+    }
     public static void main(String[] args){
 
     }
