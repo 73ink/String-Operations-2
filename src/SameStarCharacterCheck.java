c84463de7151ef955b7dac993b617e0fbadecc05
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 09:58:14 +0400

    Implement surrounding character check in Task 16

--- src/SameStarCharacterCheck.java
+++ src/SameStarCharacterCheck.java
@@ -1,6 +1,11 @@
 public class SameStarCharacterCheck {
     public static boolean sameStarChar(String originalText) {
         for (int index = 1; index < originalText.length() - 1; index++) {
+            if (originalText.charAt(index) == '*') {
+                if (originalText.charAt(index - 1) != originalText.charAt(index + 1)) {
+                    return false;
+                }
+            }
         }
         return true;
     }
