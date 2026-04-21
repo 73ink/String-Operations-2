0f359cccf098ad9044eb48f1f16b4480b4bdc19b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:38:32 +0400

    Add while loop structure for Task 20

--- src/PlusOutTextMask.java
+++ src/PlusOutTextMask.java
@@ -1,6 +1,11 @@
 public class PlusOutTextMask {
     public static String plusOut(String originalText, String keepWord) {
         String resultText = "";
+        int index = 0;
+
+        while (index < originalText.length()) {
+
+        }
         return resultText;
     }
     public static void main (String[] args){
