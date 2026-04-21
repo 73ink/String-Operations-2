40f8b5dd0e507a17279aaf1f5694dff954951929
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:37:28 +0400

    Add plusOut method for Task 20

--- src/PlusOutTextMask.java
+++ src/PlusOutTextMask.java
@@ -1,4 +1,8 @@
 public class PlusOutTextMask {
+    public static String plusOut(String originalText, String keepWord) {
+        String resultText = "";
+        return resultText;
+    }
     public static void main (String[] args){
 
     }
