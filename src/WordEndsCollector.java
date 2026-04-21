8e95e11302d001dea40385f6f1153e6ee307d434
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-21 10:42:28 +0400

    Add wordEnds method for Task 21

--- src/WordEndsCollector.java
+++ src/WordEndsCollector.java
@@ -1,4 +1,8 @@
 public class WordEndsCollector {
+    public static String wordEnds(String originalText, String targetWord) {
+        String collectedText = "";
+        return collectedText;
+    }
     public static void main (String[] args){
 
     }
